126609efc0d11954255734c428fd40c5a7fa9253
Jihyun An <jihyunan42@gmail.com>
2020-02-11 14:15:53 +0100

    tried out the latest suggestion from Jennie on Stack

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,68 +4,26 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-var x = vegetarian
-var y = hawaiian
-var z = pepperoni
-
-alert("Hey! Happy to serve you pizza! On our menu we have " + x + ", " + y + " and " + z + ".");
+alert(`Hey! Happy to serve you pizza! On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni} .`);
 
 
-let orderName = prompt("Enter the name of the pizza you want to order today.");
-
-
-if (orderName.toLowerCase() == "vegetarian") {
-  modifiedName = x;
-} else if (orderName.toLowerCase() == "vegetarian pizza") {
-  modifiedName = x;
-} else if (orderName.toLowerCase() == "hawaiian") {
-  modifiedName = y;
-} else if (orderName.toLowerCase() == "hawaiian pizza") {
-  modifiedName = y;
-} else if (orderName.toLowerCase() == "hawaian") {
-  modifiedName = y;
-} else if (orderName.toLowerCase() == "hawaian pizza") {
-  modifiedName = y;
-} else if (orderName.toLowerCase() == "pepperoni") {
-  modifiedName = z;
-} else if (orderName.toLowerCase() == "pepperoni pizza") {
-  modifiedName = z;
-} else if (orderName.toLowerCase() == "peperoni") {
-  modifiedName = z;
-} else if (orderName.toLowerCase() == "peperoni pizza") {
-  modifiedName = z;
-} else {
-  alert('Select a pizza from the menu');
+const checkOrder = () => {
   let orderName = prompt("Enter the name of the pizza you want to order today.");
-  if (orderName.toLowerCase() == "vegetarian") {
-    modifiedName = x;
-  } else if (orderName.toLowerCase() == "vegetarian pizza") {
-    modifiedName = x;
-  } else if (orderName.toLowerCase() == "hawaiian") {
-    modifiedName = y;
-  } else if (orderName.toLowerCase() == "hawaiian pizza") {
-    modifiedName = y;
-  } else if (orderName.toLowerCase() == "hawaian") {
-    modifiedName = y;
-  } else if (orderName.toLowerCase() == "hawaian pizza") {
-    modifiedName = y;
-  } else if (orderName.toLowerCase() == "pepperoni") {
-    modifiedName = z;
-  } else if (orderName.toLowerCase() == "pepperoni pizza") {
-    modifiedName = z;
-  } else if (orderName.toLowerCase() == "peperoni") {
-    modifiedName = z;
-  } else if (orderName.toLowerCase() == "peperoni pizza") {
-    modifiedName = z;
+  if (orderName.toLowerCase() == "vegetarian" || orderName.toLowerCase() == "vegetarian pizza") {
+    let orderQuantity = prompt(`How many of ${vegetarian} do you want?`);
+  } else if (orderName.toLowerCase() == "hawaiian" || orderName.toLowerCase() == "hawaiian pizza" || orderName.toLowerCase() == "hawaian" || orderName.toLowerCase() == "hawaian pizza") {
+    let orderQuantity = prompt(`How many of ${hawaiian} do you want?`);
+  } else if (orderName.toLowerCase() == "pepperoni" || orderName.toLowerCase() == "pepperoni pizza" || orderName.toLowerCase() == "peperoni" || orderName.toLowerCase() == "peperoni pizza") {
+    let orderQuantity = prompt(`How many of ${pepperoni} do you want?`);
   } else {
-    alert('Select a pizza from the menu');
+    alert('Select a pizza from the menu.');
+    checkOrder();
   }
-}
+};
 
+checkOrder();
 
-const orderQuantity = prompt("How many of " + modifiedName + " do you want?");
-
-const totalCost = orderQuantity * 80;
+let totalCost = orderQuantity * 80;
 
 if (orderQuantity < 3) {
   cookingTime = "10";
@@ -73,16 +31,14 @@ if (orderQuantity < 3) {
   cookingTime = "15";
 } else {
   cookingTime = "20";
-} 
+}
 
-console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
+document.getElementById("final-order").innerHTML = (`Great, I'll get started on your ${orderName} right away. It will cost you ${totalCost}kr. The pizzas will take ${cookingTime} minutes to prepare.`);
+
 console.log(orderName);
 console.log(orderQuantity);
-console.log(`The customer has ordered ${orderQuantity} of ${orderName}.`);
-console.log('It will cost ' + orderQuantity * 80 + 'kr.');
-console.log('It will take ' + cookingTime + ' to prepare.')
-
-document.getElementById("final-order").innerHTML = ("Great, I'll get started on your " + modifiedName + " right away, it will cost you " + totalCost + "kr. The pizzas will take " + cookingTime + " minutes.");
+console.log(`It will cost ${totalCost}kr.`);
+console.log(`It will take ${cookingTime} minutes to prepare.`)
 
 
 
