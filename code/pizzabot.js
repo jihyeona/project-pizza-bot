3934412250a2be6777f7ec8ce56a82fb31b38c2e
Jihyun An <jihyunan42@gmail.com>
2020-02-14 09:40:48 +0100

    updated version after tech call with Van. dealt with scope issues

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,15 +6,29 @@ const pizzaPrice = 80
 
 alert(`Hey! Happy to serve you pizza! On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni} .`);
 
+const finalOrder = (name, quantity) => {
+  let totalCost = quantity * 80;
+  if (quantity < 3) {
+    cookingTime = "10";
+  } else if (quantity < 6) {
+    cookingTime = "15";
+  } else {
+    cookingTime = "20";
+  };
+  document.getElementById("final-order").innerHTML = (`Great, I'll get started on your ${name} right away. It will cost you ${totalCost}kr. The pizzas will take ${cookingTime} minutes to prepare.`)
+};
 
 const checkOrder = () => {
   let orderName = prompt("Enter the name of the pizza you want to order today.");
   if (orderName.toLowerCase() == "vegetarian" || orderName.toLowerCase() == "vegetarian pizza") {
     let orderQuantity = prompt(`How many of ${vegetarian} do you want?`);
+    finalOrder(orderName, orderQuantity);
   } else if (orderName.toLowerCase() == "hawaiian" || orderName.toLowerCase() == "hawaiian pizza" || orderName.toLowerCase() == "hawaian" || orderName.toLowerCase() == "hawaian pizza") {
     let orderQuantity = prompt(`How many of ${hawaiian} do you want?`);
+    finalOrder(orderName, orderQuantity);
   } else if (orderName.toLowerCase() == "pepperoni" || orderName.toLowerCase() == "pepperoni pizza" || orderName.toLowerCase() == "peperoni" || orderName.toLowerCase() == "peperoni pizza") {
     let orderQuantity = prompt(`How many of ${pepperoni} do you want?`);
+    finalOrder(orderName, orderQuantity);
   } else {
     alert('Select a pizza from the menu.');
     checkOrder();
@@ -23,22 +37,11 @@ const checkOrder = () => {
 
 checkOrder();
 
-let totalCost = orderQuantity * 80;
 
-if (orderQuantity < 3) {
-  cookingTime = "10";
-} else if (orderQuantity < 6) {
-  cookingTime = "15";
-} else {
-  cookingTime = "20";
-}
 
-document.getElementById("final-order").innerHTML = (`Great, I'll get started on your ${orderName} right away. It will cost you ${totalCost}kr. The pizzas will take ${cookingTime} minutes to prepare.`);
 
-console.log(orderName);
-console.log(orderQuantity);
-console.log(`It will cost ${totalCost}kr.`);
-console.log(`It will take ${cookingTime} minutes to prepare.`)
+
+
 
 
 
